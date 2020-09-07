d37977c59c96dbe32581cb0cb9b26fd4cf33075c
EACUAMBA <EdilconCuamba.27101999>
2020-09-07 13:41:11 +0200

    Primeiro comite do dia 07 de Setembro de 2020

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Gestão_de_Emprestimos.DAO;
+using Gestão_de_Emprestimos.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestão_de_Emprestimos
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            DataSourceInfoFileXMLDAO dataSourceInfoFileXMLDAO = new DataSourceInfoFileXMLDAO();
+            Boolean result = dataSourceInfoFileXMLDAO.readXMLFile() != null;
+            if (result)
+            {
+                if (File.Exists(dataSourceInfoFileXMLDAO.readXMLFile().Path))
+                {
+                    Application.Run(new FormMain());
+
+                }else
+                {
+                    Application.Run(new InputMessageDialog());
+                }
+            }
+            else
+            {
+                Application.Run(new InputMessageDialog());
+            }
+            
+                
+            
+        }
+    }
+}
